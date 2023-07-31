e2bdb7fad3ecb5b9f81db603da468e379b775ea1
Ben D <bendickinson2002@gmail.com>
2023-07-31 12:17:35 +1000

    Delete src directory

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Footer } from "./Footer";
-export { default as Home } from "./Home";
-export { default as FindDev } from "./findDev";
-export { default as FindJob } from "./findJob";
-export { default as Blog } from "./blog/Blog";
-export { default as Posts } from "./blog/Posts";
-export { default as Post } from "./blog/Post";
